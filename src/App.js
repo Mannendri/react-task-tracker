17141f59a9380096cff0593ee38b3d6e5eb59919
Mannendri Olivares <mannendriolivares@Mannendris-MBP.lan>
2021-05-02 13:28:48 -0400

    Added UI functionality for adding and removing tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import { useState } from 'react'
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
 
 function App() {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      text:'Doctor\'s Appointment',
+      day: 'Feb 5th at 2:30 pm',
+      reminder:true,
+    },
+    {
+      id:2,
+      text:'Meeting at School',
+      day: 'Feb 6th at 1:30 pm',
+      reminder:true,
+    }
+  ])
+  //Add task
+  const addTask = (task) =>{
+    const id = Math.floor(Math.random()*10000)+1
+    const newTask = {id, ...task}
+    setTasks([...tasks,newTask])
+  }
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task)=>task.id!==id))
+  }
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id==id ? {...task, reminder: !task.reminder}: task))
+  }
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      <AddTask onAdd={addTask}/>
+      {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      : 'No tasks!'}
     </div>
   );
 }
